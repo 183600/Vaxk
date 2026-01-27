467b02342c83d1df9a2edf35bd2728a20d504835
183600 <zhongwenjia2009@163.com>
2026-01-28 00:40:04 +0800

    vfnnadjk

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 module Main where
 
 import Options.Applicative
@@ -38,11 +39,18 @@ data Command
   | Build { path :: Text}
   deriving (Show)
 
-data Token
+data SmallPiece
   = Package { name :: Maybe Text}
   , Import { name :: Maybe Text}
   , Func { name :: Maybe Text}
   deriving (Show)
+
+data ParseLine{
+  smallPieceVector :: V.Vector Text SmallPiece,
+  remainingTokens :: [Text],
+  allTokens :: [Text],
+  i :: Int,
+  } deriving (Show)
 
 -- 2. 实现具体的业务逻辑
 runApp :: Options -> IO ()
@@ -55,24 +63,25 @@ runApp (Options cmd) = case cmd of
       let lineList = T.lines contents
     -- 3. 逐行处理 (例如打印行号和内容)
       -- 方案：遍历每一行，对每一行 words，然后 flatten（压平），最后处理
-      let allWords = map T.words linelist
+      let allWords = split [';' , ',' , '.' , ':' , '|' , '&' , '^' , '+' , '-' , '*' , '/' , '%' , '<' , '>' , '=' , '!' , '(' , ')' , '[' , ']' , '{' , '}' , '_' , '"' , "'" , '`'] $ map T.words linelist
       -- allWords 的类型是 [Text]
       -- 使用 mapM_ 执行 IO
       mapM_ parser allWords
 
-parseLine :: V.Vector Text Token -> [Text] -> [Text] -> Int -> [Token]
+parseLine :: V.Vector Text SmallPiece -> [Text] -> [Text] -> Int -> [SmallPiece]
 parseLine _ [] _ _= (Nothing,Text)
 parseLine h fullList@("package":xs) a b = parseType Package fullList
 parseLine h fullList@("import":xs) a b = parseType Import fullList
 parseLine h fullList@("func":xs) a b = parseType Func fullList
+parseLine h fullList@(['"']:xs) a b = parseType Func fullList
 parseLine h (x:xs) a b
   | a[b-1] == "package" = h V.// [(V.length , Package{name=a !! $ length a})]
-  | a[b:1] == "import" = h V.// [(V.length , Import{name=parserGoString a ['"']})]
-  | a[b-1] == "func" = T.break (=='(') x
+  | a[b-2] == "import" = h V.// [(V.length , Import{name=a})]
+  | a[b-1] == "func" = h V.// [(V.length , Func{name=x})]
   where
     parseType constructor fullList = parseLine (V.snoc h constructor{name=Nothing}) xs fullList a+1
 
-parser' :: V.Vector Text Token -> [[Text]] -> [Maybe Token]
+parser' :: V.Vector Text SmallPiece -> [[Text]] -> [Maybe SmallPiece]
 parser' _ [] = (Nothing,Text)
 parser' h (x:xs) = parseLine V.fromList[] (x <> parser' xs) (x <> parser' xs) 0
 
@@ -81,13 +90,9 @@ parser x = ()
   where
     parser' V.fromList[] x
 
-parserGoString :: [Text] -> [Text] -> Maybe Text
-parserGoString a [] = Nothing
-parserGoString a (x:xs) = content
-  where
-    case between x x (a !! $ length a) of
-        Just result -> let content = result -- 输出: Target Data
-        Nothing     -> let content = parserGoString a xs
+split :: [Text] -> [Text] -> Maybe Text
+split [] a = a
+split (x:xs) a = split xs map (map (T.splitOn x)) linelist
 
 -- 解析子命令
 runParser :: Parser Command
